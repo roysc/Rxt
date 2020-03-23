803c3773f73768f0e154f4ed4dd7f8852f7cba01
Roy Crihfield <roy.crihfield@gmail.com>
2020-03-22 23:19:04 -0500

    Fix SFINAE for ostream operator<<

--- src/Rxt/graphics/glm.hpp
+++ src/Rxt/graphics/glm.hpp
@@ -4,25 +4,29 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/string_cast.hpp>
 
-#include <boost/predef.h>
 #include <experimental/type_traits>
 #include <iostream>
 #include <type_traits>
 
 namespace Rxt
 {
-#if !BOOST_COMP_GNUC
 template <class T>
-using glm_string_castable = decltype(glm::to_string(std::declval<T>()));
+using has_glm_to_string = decltype(glm::to_string(std::declval<T>()));
+
+template <class Ch, class Tr, class T>
+using has_stream_operator =
+    decltype(std::declval<std::basic_ostream<Ch, Tr>>() << std::declval<T>());
 }
 
 namespace std
 {
 template <class Ch, class Tr, class T,
-          class = enable_if_t<std::experimental::is_detected_v<Rxt::glm_string_castable, T>>>
+          enable_if_t<
+              !experimental::is_detected_v<Rxt::has_stream_operator, Ch, Tr, T> &&
+              experimental::is_detected_v<Rxt::has_glm_to_string, T>
+              >* = nullptr>
 basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& o, T const& x)
 {
     return o << glm::to_string(x);
 }
-#endif
 }
