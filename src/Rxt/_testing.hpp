7e68934851e6aea11928693483d4953b8a0ef41f
Roy Crihfield <roy.crihfield@gmail.com>
2020-06-13 02:01:00 -0500

    Test additions
    use doctest
    
    add for time.hpp
    
    refactors

--- src/Rxt/_testing.hpp
+++ src/Rxt/_testing.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
---
+++ src/Rxt/time.test.cpp
@@ -0,0 +1,10 @@
+#include "time.hpp"
+#include "_testing.hpp"
+
+namespace Rxt {
+TEST_CASE("duration_fps")
+{
+    auto d = duration_fps<30>(1);
+    REQUIRE(d.count() == 1);
+}
+}
