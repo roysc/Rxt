e838c58e98cb722742f3b7d6132ac4ab28313346
Roy Crihfield <roy@manteia.ltd>
2020-07-07 23:48:57 -0500

    [wip] sdl test

--- src/Rxt/graphics/sdl.test.cpp
+++ src/Rxt/graphics/sdl.test.cpp
@@ -2,27 +2,24 @@
 #include "../_testing.hpp"
 #include <chrono>
 
+extern "C" void step(void*);
+
 namespace Rxt::sdl
 {
-struct ctx : simple_gui
+struct TestCtx : simple_gui
 {
-    ctx() : simple_gui("test", 10,10) {}
+    TestCtx() : simple_gui("test", 10,10) {}
     bool is_stopped() const { return true; }
     void advance(SDL_Event e) {}
 };
-extern "C" void step(void* c)
-{
-    sdl::em_advance<ctx>(c);
-}
-using test_ctx = ctx;
 
 TEST_CASE("simple_gui")
 {
-    auto loop = sdl::em_looper(new test_ctx(), step);
+    auto loop = sdl::em_looper(new TestCtx(), step);
     loop();
 }
 
-struct ctx_met : test_ctx
+struct TestCtxMet : TestCtx
 {
     using dur = std::chrono::duration<float, std::ratio<1, 30>>;
     metronome m2{dur(), [this] { return !is_stopped(); }};
@@ -30,7 +27,13 @@ struct ctx_met : test_ctx
 
 TEST_CASE("metronome")
 {
-    auto loop = sdl::em_looper(new ctx_met(), step);
+    auto loop = sdl::em_looper(new TestCtxMet(), step);
     loop();
 }
+}
+
+extern "C" void step(void* c)
+{
+    using namespace Rxt::sdl;
+    em_advance<TestCtx>(c);
 }
