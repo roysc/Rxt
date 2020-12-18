8a46c51b516a96e7a88f5b76d8b8d2300cf2dd37
Roy Crihfield <roy@manteia.ltd>
2020-12-18 22:44:44 +0800

    fix image load

--- src/Rxt/graphics/image.hpp
+++ src/Rxt/graphics/image.hpp
@@ -15,7 +15,7 @@ namespace Rxt
 inline auto load_image(const char* path)
 {
     int width, height, nchannels;
-    auto image = SOIL_load_image(path, &width, &height, &nchannels, SOIL_LOAD_RGB);
+    auto image = SOIL_load_image(path, &width, &height, &nchannels, SOIL_LOAD_AUTO);
     if (image) {
         RXT_info("Loaded image: [w, h, nc]=[{0},{1},{2}]\n", width, height, nchannels);
     } else {
