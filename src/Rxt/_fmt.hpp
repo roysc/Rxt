c84123f6b11f17e1e6e27349266a9703471fdc62
Roy Crihfield <roy@manteia.ltd>
2021-05-11 17:13:28 +0800

    cleanup io/fmt

--- src/Rxt/_fmt.hpp
+++ src/Rxt/_fmt.hpp
@@ -1,41 +1,21 @@
 #pragma once
 
-#ifndef RXT_DISABLE_FMT
-    #define FMT_STRING_ALIAS 1
-    #include <fmt/format.h>
-    #include <fmt/ostream.h>
-#else
-    #include <iostream>
-#endif
+#define FMT_STRING_ALIAS 1
+#include <fmt/format.h>
+#include <fmt/ostream.h>
 
 #include <utility>
 #include <cstdio>
 
 namespace Rxt
 {
-#ifndef RXT_DISABLE_FMT
-namespace _fmt = ::fmt;
-using _fmt::print;
+// namespace _fmt = ::fmt;
+using ::fmt::print;
+using ::fmt::format;
 
 template <class...T>
 void print_to(std::FILE* out, T&&...a)
 {
     print(out, std::forward<T...>(a)...);
 }
-#else
-namespace _fmt {}
-
-template <class... T>
-void print_to(std::ostream& out, const char* m, T&&... args)
-{
-    out << m;
-    (out << ... << args << " ");
-}
-
-template <class... T>
-void print(const char* m, T&&... args)
-{
-    print_to(std::cout, std::forward<T...>(args)...);
-}
-#endif
 }
--- src/Rxt/io.hpp
+++ src/Rxt/io.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "_fmt.hpp"
+#ifndef RXT_DISABLE_FMT
+    #include "_fmt.hpp"
+#else
+    #include <iostream>
+#endif
 
 #include <string>
 #include <fstream>
@@ -8,6 +12,21 @@
 
 namespace Rxt
 {
+#ifdef RXT_DISABLE_FMT
+template <class... T>
+void print_to(std::ostream& out, const char* m, T&&... args)
+{
+    out << m;
+    (out << ... << args << " ");
+}
+
+template <class... T>
+void print(const char* m, T&&... args)
+{
+    print_to(std::cout, std::forward<T...>(args)...);
+}
+#endif
+
 inline std::string read_file(std::string const& p)
 {
     std::ifstream in{p};
