d2b431cbd136e778ef85d95ab88ee17a04e5b979
Roy Crihfield <roy@manteia.ltd>
2021-05-18 02:06:40 +0800

    rf data_traits

--- src/Rxt/graphics/gl_data.cpp
+++ src/Rxt/graphics/gl_data.cpp
@@ -9,99 +9,119 @@ namespace Rxt::gl::_det
 
 #define _span_ptr(v_)( glm::value_ptr((v_)[0]))
 
-void data_traits<GLint>::set(value_type id, GLint val)
+template <>
+void data_setter<GLint>::set_value(value_type id, GLint val)
 {
     glUniform1i(id, val);
 }
 
-void data_traits<GLint>::set_array(value_type id, span<GLint const> val)
+template <>
+void data_setter<GLint>::set_array(value_type id, span<GLint const> val)
 {
     glUniform1iv(id, val.size(), val.data());
 }
 
-void data_traits<GLuint>::set(value_type id, GLint val)
+template <>
+void data_setter<GLuint>::set_value(value_type id, GLuint val)
 {
     glUniform1ui(id, val);
 }
 
-void data_traits<GLuint>::set_array(value_type id, span<GLuint const> val)
+template <>
+void data_setter<GLuint>::set_array(value_type id, span<GLuint const> val)
 {
     glUniform1uiv(id, val.size(), val.data());
 }
 
-void data_traits<fvec2>::set(value_type id, fvec2 const& v)
+using namespace vec;
+
+template <>
+void data_setter<fvec2>::set_value(value_type id, fvec2 v)
 {
     glUniform2f(id, v.x, v.y);
 }
 
-void data_traits<fvec2>::set_array(value_type id, span<fvec2 const> v)
+template <>
+void data_setter<fvec2>::set_array(value_type id, span<fvec2 const> v)
 {
     glUniform2fv(id, v.size(), _span_ptr(v));
 }
 
-void data_traits<ivec2>::set(value_type id, ivec2 const& v)
+template <>
+void data_setter<ivec2>::set_value(value_type id, ivec2 v)
 {
     glUniform2i(id, v.x, v.y);
 }
 
-void data_traits<ivec2>::set_array(value_type id, span<ivec2 const> v)
+template <>
+void data_setter<ivec2>::set_array(value_type id, span<ivec2 const> v)
 {
     glUniform2iv(id, v.size(), _span_ptr(v));
 }
 
-void data_traits<uvec2>::set(value_type id, uvec2 const& v)
+template <>
+void data_setter<uvec2>::set_value(value_type id, uvec2 v)
 {
     glUniform2ui(id, v.x, v.y);
 }
 
-void data_traits<uvec2>::set_array(value_type id, span<uvec2 const> v)
+template <>
+void data_setter<uvec2>::set_array(value_type id, span<uvec2 const> v)
 {
     glUniform2uiv(id, v.size(), _span_ptr(v));
 }
 
-void data_traits<fvec3>::set(value_type id, fvec3 const& v)
+template <>
+void data_setter<fvec3>::set_value(value_type id, fvec3 v)
 {
     glUniform3f(id, v.x, v.y, v.z);
 }
 
-void data_traits<fvec3>::set_array(value_type id, span<fvec3 const> v)
+template <>
+void data_setter<fvec3>::set_array(value_type id, span<fvec3 const> v)
 {
     glUniform3fv(id, v.size(), _span_ptr(v));
 }
 
-void data_traits<fvec4>::set(value_type id, fvec4 const& v)
+template <>
+void data_setter<fvec4>::set_value(value_type id, fvec4 v)
 {
     glUniform4f(id, v.x, v.y, v.z, v.w);
 }
 
-void data_traits<fvec4>::set_array(value_type id, span<fvec4 const> v)
+template <>
+void data_setter<fvec4>::set_array(value_type id, span<fvec4 const> v)
 {
     glUniform4fv(id, v.size(), _span_ptr(v));
 }
 
-void data_traits<fmat3>::set(value_type id, fmat3 const& m)
+template <>
+void data_setter<fmat3>::set_value(value_type id, fmat3 m)
 {
     glUniformMatrix3fv(id, 1, GL_FALSE, value_ptr(m));
 }
 
-void data_traits<fmat3>::set_array(value_type id, span<fmat3 const> ms)
+template <>
+void data_setter<fmat3>::set_array(value_type id, span<fmat3 const> ms)
 {
     glUniformMatrix3fv(id, ms.size(), GL_FALSE, _span_ptr(ms));
 }
 
-void data_traits<fmat4>::set(value_type id, fmat4 const& m)
+template <>
+void data_setter<fmat4>::set_value(value_type id, fmat4 m)
 {
     glUniformMatrix4fv(id, 1, GL_FALSE, value_ptr(m));
 }
 
-void data_traits<fmat4>::set_array(value_type id, span<fmat4 const> ms)
+template <>
+void data_setter<fmat4>::set_array(value_type id, span<fmat4 const> ms)
 {
     glUniformMatrix4fv(id, ms.size(), GL_FALSE, _span_ptr(ms));
 }
 
-// // struct data_traits<bool> : _det::data_adaptor<GL_UNSIGNED_BYTE> // todo: ok?
+// // struct data_setter<bool> : _det::data_adaptor<GL_UNSIGNED_BYTE> // todo: ok?
 // {
-//     void set(value_type id, bool x)
+//     void set_value(value_type id, bool x)
 //     {
 //         glUniform1i(id, x);
 //     }
--- src/Rxt/graphics/gl_data.hpp
+++ src/Rxt/graphics/gl_data.hpp
@@ -2,8 +2,10 @@
 
 #include "gl_core.hpp"
 #include "gl_guard.hpp"
-#include "Rxt/vec.hpp"
-#include "Rxt/span.hpp"
+
+#include <Rxt/vec.hpp>
+#include <Rxt/span.hpp>
+#include <Rxt/format.hpp>
 
 namespace Rxt::gl
 {
@@ -15,42 +17,13 @@ inline const vec::fvec3
 namespace _det
 {
 template <class T>
-struct data_traits;
-}
-
-using _det::data_traits;
-
-template <class T>
-void set(uniform<T>& u, T const& x)
+struct data_setter
 {
-    gl::use_guard _p(u.program());
-    data_traits<T>::set(u, x);
-}
+    using value_type = GLuint;
+    static void set_value(value_type, T);
+    static void set_array(value_type, span<T const>);
+};
 
-template <class T>
-void set(uniform<T>& u, span<T const> xs)
-{
-    gl::use_guard _p(u.program());
-    data_traits<T>::set_array(u, xs);
-}
-
-template <class T>
-void set_at(uniform<T>& u, T const& x, unsigned i)
-{
-    auto name = format("{}[{}]", u.name(), i);
-    gl::uniform<T> ui(u.program(), name.c_str());
-    set(u, x);
-}
-
-template <class T>
-void set_uniform(program& prog, const char* n, T const& x)
-{
-    gl::uniform<T> u(prog, n);
-    set(u, x);
-}
-
-namespace _det
-{
 template <GLenum Enum, GLint D0 = 1, GLint D1 = 1>
 struct data_adaptor
 {
@@ -63,85 +36,47 @@ struct data_adaptor
         data_enum != GL_FLOAT &&
         data_enum != GL_DOUBLE;
 };
-
-using namespace vec;
-
-template <>
-struct data_traits<GLint> : _det::data_adaptor<GL_INT>
-{
-    static void set(value_type id, GLint val);
-    static void set_array(value_type id, span<GLint const> val);
-};
-
-template <>
-struct data_traits<GLuint> : _det::data_adaptor<GL_UNSIGNED_INT>
-{
-    static void set(value_type id, GLint val);
-    static void set_array(value_type id, span<GLuint const> val);
-};
-
-template <>
-struct data_traits<fvec2> : _det::data_adaptor<GL_FLOAT, 2>
-{
-    static void set(value_type id, fvec2 const& v);
-    static void set_array(value_type id, span<fvec2 const> v);
-};
-
-template <>
-struct data_traits<ivec2> : _det::data_adaptor<GL_INT, 2>
-{
-    static void set(value_type id, ivec2 const& v);
-    static void set_array(value_type id, span<ivec2 const> v);
-};
-
-template <>
-struct data_traits<uvec2> : _det::data_adaptor<GL_UNSIGNED_INT, 2>
-{
-    static void set(value_type id, uvec2 const& v);
-    static void set_array(value_type id, span<uvec2 const> v);
-};
-
-template <>
-struct data_traits<fvec3> : _det::data_adaptor<GL_FLOAT, 3>
-{
-    static void set(value_type id, fvec3 const& v);
-    static void set_array(value_type id, span<fvec3 const> v);
-};
-
-template <>
-struct data_traits<fvec4> : _det::data_adaptor<GL_FLOAT, 4>
-{
-    static void set(value_type id, fvec4 const& v);
-    static void set_array(value_type id, span<fvec4 const> v);
-};
-
-template <>
-struct data_traits<fmat3> : _det::data_adaptor<GL_FLOAT, 3, 3>
-{
-    static void set(value_type id, fmat3 const& m);
-    static void set_array(value_type id, span<fmat3 const> ms);
-};
-
-template <>
-struct data_traits<fmat4> : _det::data_adaptor<GL_FLOAT, 4, 4>
-{
-    static void set(value_type id, fmat4 const& m);
-    static void set_array(value_type id, span<fmat4 const> ms);
-};
-
-// template <>
-// struct data_traits<bool> : _det::data_adaptor<GL_UNSIGNED_BYTE> // todo: ok?
-// {
-//     static void set(value_type id, bool x)
-//     {
-//         glUniform1i(id, x);
-//     }
-
-//     static void set_array(value_type id, span<bool const> val)
-//     {
-//         glUniform1iv(id, val.size(), static_cast<int const*>(val.data()));
-//     }
-// };
-
 }
+
+template <class T>
+void set(uniform<T>& u, T const& x)
+{
+    use_guard _p(u.program());
+    _det::data_setter<T>::set_value(u, x);
+}
+
+template <class T>
+void set_array(uniform<T>& u, span<T> xs)
+{
+    use_guard _p(u.program());
+    _det::data_setter<T>::set_array(u, xs);
+}
+
+template <class T>
+void set_index(uniform<T>& u, unsigned i, T x)
+{
+    auto name = format("{}[{}]", u.name(), i);
+    set_uniform(u.program(), name.c_str(), x);
+}
+
+template <class T>
+void set_uniform(program& prog, const char* n, T const& x)
+{
+    uniform<T> u(prog, n);
+    set(u, x);
+}
+
+template <class T>
+struct data_traits;
+
+template <> struct data_traits<GLint> : _det::data_adaptor<GL_INT> {};
+template <> struct data_traits<GLuint> : _det::data_adaptor<GL_UNSIGNED_INT> {};
+template <> struct data_traits<vec::fvec2> : _det::data_adaptor<GL_FLOAT, 2> {};
+template <> struct data_traits<vec::ivec2> : _det::data_adaptor<GL_INT, 2> {};
+template <> struct data_traits<vec::uvec2> : _det::data_adaptor<GL_UNSIGNED_INT, 2> {};
+template <> struct data_traits<vec::fvec3> : _det::data_adaptor<GL_FLOAT, 3> {};
+template <> struct data_traits<vec::fvec4> : _det::data_adaptor<GL_FLOAT, 4> {};
+template <> struct data_traits<vec::fmat3> : _det::data_adaptor<GL_FLOAT, 3, 3> {};
+template <> struct data_traits<vec::fmat4> : _det::data_adaptor<GL_FLOAT, 4, 4> {};
+// template <> struct data_traits<bool> : _det::data_adaptor<GL_UNSIGNED_BYTE> {};
 }
