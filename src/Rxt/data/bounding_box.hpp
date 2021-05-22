5530112dd7709a50a46a88caf1c05022c96b8abf
Roy Crihfield <roy@manteia.ltd>
2021-05-22 21:20:25 +0800

    bounding_box.hpp

--- src/Rxt/data/bounding_box.hpp
+++ src/Rxt/data/bounding_box.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Rxt/vec.hpp>
+#include <Rxt/_fmt.hpp>
+
 #include <utility>
 
 namespace Rxt
@@ -9,11 +11,11 @@ template <class FT>
 struct bounding_box
 {
     using vec_type = vec::tvec2<FT>;
-    vec_type _min, _max;
+    vec_type m_min, m_max;
 
     bounding_box(vec_type a, vec_type b)
-        : _min(min(a, b))
-        , _max(max(a,b ))
+        : m_min(min(a, b))
+        , m_max(max(a, b))
     {}
 
     bounding_box(std::pair<vec_type, vec_type> pair)
@@ -21,23 +23,60 @@ struct bounding_box
 
     bounding_box() : bounding_box(vec_type(), vec_type()) {}
 
-    auto shape() const { return _max - _min; }
-    auto center() const { return _min + shape() / FT(2); }
-
-    auto left() const { return _min; }
-    auto right() const { return _max; }
-
-    bool contains(const bounding_box& box) const
+    static bounding_box with_length(vec_type a, vec_type l)
     {
-        return all(lessThanEqual(left(), box.left())) && all(lessThanEqual(box.right(), right()));
+        return bounding_box(a, a + l);
     }
 
-    bool intersects(const bounding_box& box) const
+    auto min_corner() const { return m_min; }
+    auto max_corner() const { return m_max; }
+    auto left() const { return min_corner(); }
+    auto right() const { return max_corner(); }
+
+    auto shape() const { return m_max - m_min; }
+    auto center() const { return m_min + shape() / FT(2); }
+
+    bool contains(const bounding_box& that) const
     {
-        return !any(greaterThanEqual(left(), box.right()) || greaterThanEqual(box.left(), right()));
+        return all(
+            lessThanEqual(min_corner(), that.min_corner()) &&
+            lessThanEqual(that.max_corner(), max_corner())
+        );
+    }
+
+    bool contains(vec_type point) const
+    {
+        return all(
+            lessThanEqual(min_corner(), point) &&
+            lessThanEqual(point, max_corner())
+        );
+    }
+
+    bool intersects(const bounding_box& that) const
+    {
+        return !any(
+            greaterThan(min_corner(), that.max_corner()) ||
+            greaterThan(that.min_corner(), max_corner())
+        );
     }
 };
 
 template <class FT>
 bounding_box(vec::tvec2<FT>, vec::tvec2<FT>) -> bounding_box<FT>;
 }
+
+template <class FT>
+struct fmt::formatter<Rxt::bounding_box<FT>>
+{
+    template <class PC>
+    constexpr auto parse(PC& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template <class FC>
+    auto format(const Rxt::bounding_box<FT>& val, FC& ctx)
+    {
+        return fmt::format_to(ctx.out(), "bounding_box({}, {})", val.min_corner(), val.max_corner());
+    }
+};
--- src/Rxt/util.hpp
+++ src/Rxt/util.hpp
@@ -22,7 +22,7 @@ template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 auto get_first = [] (auto&& pair) -> auto&& { return (pair.first); };
 auto get_second = [] (auto&& pair) -> auto&& { return (pair.second); };
 
-// "Lazy" function wrapper to batch calls and flush manually
+// Function wrapper to batch calls and flush manually
 struct lazy_action
 {
     using action_function = std::function<void()>;
@@ -58,40 +58,4 @@ struct runtime //todo nix
 };
 
 using simple_runtime = runtime;
-
-template <class FT>
-struct bounding_box
-{
-    using vec_type = vec::tvec2<FT>;
-    vec_type _min, _max;
-
-    bounding_box(vec_type a, vec_type b)
-        : _min(min(a, b))
-        , _max(max(a,b ))
-    {}
-
-    bounding_box(std::pair<vec_type, vec_type> pair)
-        : bounding_box(pair.first, pair.second) {}
-
-    bounding_box() : bounding_box(vec_type(), vec_type()) {}
-
-    auto shape() const { return _max - _min; }
-    auto center() const { return _min + shape() / FT(2); }
-
-    auto left() const { return _min; }
-    auto right() const { return _max; }
-
-    bool contains(const bounding_box& box) const
-    {
-        return all(lessThanEqual(left(), box.left())) && all(lessThanEqual(box.right(), right()));
-    }
-
-    bool intersects(const bounding_box& box) const
-    {
-        return !any(greaterThanEqual(left(), box.right()) || greaterThanEqual(box.left(), right()));
-    }
-};
-
-template <class FT>
-bounding_box(vec::tvec2<FT>, vec::tvec2<FT>) -> bounding_box<FT>;
 }
