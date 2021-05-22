de81e2c09065d152bd6fdea206ce72a831deb7f6
Roy Crihfield <roy@manteia.ltd>
2021-05-22 21:20:47 +0800

    vec utilities
    product, squared_length, unravel_index

--- src/Rxt/vec.hpp
+++ src/Rxt/vec.hpp
@@ -7,5 +7,42 @@ namespace Rxt
 namespace vec
 {
 using namespace glm;
+
+template <int L, class T>
+using basic_vec = glm::vec<L, T, glm::highp>;
+
+template <int L, class T>
+constexpr auto squared_length(basic_vec<L, T> const& v)
+{
+    T sum = 0;
+    for (int i{}; i < L; ++i)
+        sum += v[i] * v[i];
+    return sum;
+}
+
+template <int L, class T>
+constexpr auto product(basic_vec<L, T> const& v)
+{
+    T prod = 1;
+    for (int i{}; i < L; ++i)
+        prod *= v[i];
+    return prod;
+}
+
+template <int L, class T>
+constexpr auto unravel_index(unsigned ix, basic_vec<L, T> const& shape)
+{
+    if constexpr (L == 1) {
+        return basic_vec<L,T>(ix);
+    } else {
+        // shape excluding last axis
+        basic_vec<L-1,T> slice_shape(shape);
+        auto slice_area = product(slice_shape);
+        return basic_vec<L,T>(
+            unravel_index(ix % slice_area, slice_shape),
+            ix / slice_area
+        );
+    }
+}
 }
 }
--- src/Rxt/vec.test.cpp
+++ src/Rxt/vec.test.cpp
@@ -4,23 +4,46 @@
 #include <iostream>
 #include <unordered_set>
 
-namespace Rxt
+namespace Rxt::vec
 {
-TEST_CASE("fvec2")
+TEST_CASE("basic")
 {
-    vec::fvec2 v(0), u(1);
-    REQUIRE_EQ(v + u, u);
+    {
+        fvec2 v(0), u(1);
+        REQUIRE_EQ(v + u, u);
+    }
+    {
+        fvec3 v(1,2,3);
+        REQUIRE_EQ(squared_length(v), 14);
+        REQUIRE_EQ(product(v), 6);
+        ivec3 vi(1,2,3);
+        REQUIRE_EQ(squared_length(vi), 14);
+        REQUIRE_EQ(product(vi), 6);
+    }
 }
 
 TEST_CASE("std::hash definitions")
 {
-    std::unordered_set<vec::fvec2> s;
+    std::unordered_set<fvec2> s;
     s.emplace(0);
 }
 
-// TEST_CASE("stream operator")
-// {
-//     fvec2 v(1);
-//     std::cout << "vec = " << v << "\n";
-// }
+TEST_CASE("unravel_index")
+{
+    uvec1 shape1(3);
+    REQUIRE_EQ(unravel_index(2, shape1), uvec1(2));
+
+    uvec2 shape2(3);
+    REQUIRE_EQ(unravel_index(4, shape2), uvec2(1, 1));
+
+    shape2 = uvec2(4,1);
+    REQUIRE_EQ(unravel_index(3, shape2), uvec2(3, 0));
+    shape2 = uvec2(1,4);
+    REQUIRE_EQ(unravel_index(3, shape2), uvec2(0, 3));
+
+    uvec3 shape3(3);
+    REQUIRE_EQ(unravel_index(10, shape3), uvec3(1,0,1));
+
+    // print("i={} shape={} unravelled={}\n",unravel_index(3, shape));
+}
 }
